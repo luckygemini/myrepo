190763e0832f55ce6bff2ad2f355f2ae3c632bac
陈勇 <chenyong@chenyongdeMacBook-Pro.local>
2014-08-16 15:48:15 +0800

    create HelloWorld1 !

---
+++ HelloWorld1/HelloWorld1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld1
+//
+//  Created by 陈勇 on 14-8-16.
+//  Copyright (c) 2014年 sytc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
